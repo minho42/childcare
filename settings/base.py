3697e74b909690099ed3a6bb90054dc6e3b8d035
Min ho Kim <minho42@gmail.com>
2023-06-12 11:20:56 +1000

    add allowed host

--- settings/base.py
+++ settings/base.py
@@ -15,10 +15,7 @@ SECRET_KEY = env("CHILDCAREAPP_SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "childcare.up.railway.app"]
 # <-- added for bebug_toolbar to appear
 INTERNAL_IPS = ["localhost", "127.0.0.1"]
 
