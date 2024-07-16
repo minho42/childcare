b129c4665eab0447282b6704d9f8d4b574f90d7d
Min ho Kim <minho42@gmail.com>
2024-07-16 11:29:15 +1000

    upup

--- settings/base.py
+++ settings/base.py
@@ -15,7 +15,7 @@ SECRET_KEY = env("CHILDCAREAPP_SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "childcare.up.railway.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 # <-- added for bebug_toolbar to appear
 INTERNAL_IPS = ["localhost", "127.0.0.1"]
 
@@ -96,12 +96,11 @@ SITE_ID = 1
 
 WSGI_APPLICATION = "project.wsgi.application"
 
-# https://github.com/jazzband/dj-database-url/
 DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-    ),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
