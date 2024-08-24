6699f4da83ae00454f04a1b2dbc72ae6658fdaaa
Min ho Kim <minho42@gmail.com>
2024-08-25 08:26:21 +1000

    terminate all processes at the end

--- auto_db_update.sh
+++ auto_db_update.sh
@@ -8,14 +8,17 @@ source .venv/bin/activate
 
 echo "starting django server: childcare"
 nohup python3 manage.py runserver >django.log 2>&1 &
+DJANGO_PID=$!
 
 if redis-cli ping | grep -q "PONG"; then
     echo "redis is running: 🏓 PONG"
 
     echo "starting celery worker: childcare"
     nohup celery -A project worker --beat --scheduler django --loglevel=info >celery.log 2>&1 &
+    CELERY_PID=$!
 
     tail -f celery.log &
+    TAIL_PID=$!
 else
     echo "redis not running, celery will not start"
     exit 1
@@ -30,3 +33,10 @@ git push
 
 echo "cleaning up logs..."
 trash django.log celery.log
+
+echo "terminating processes..."
+kill $DJANGO_PID
+kill $CELERY_PID
+kill $TAIL_PID
+
+echo "done"
