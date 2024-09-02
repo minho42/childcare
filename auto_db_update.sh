45ead89d88a54f7892b812dca4e91728f211b76d
Min ho Kim <minho42@gmail.com>
2024-09-03 01:01:14 +1000

    update

--- auto_db_update.sh
+++ auto_db_update.sh
@@ -7,7 +7,7 @@ echo "childcare: auto_db_update.sh"
 source .venv/bin/activate
 
 echo "starting django server: childcare"
-nohup python3 manage.py runserver >django.log 2>&1 &
+nohup python3 manage.py runserver 8999 >django.log 2>&1 &
 DJANGO_PID=$!
 
 /opt/homebrew/bin/redis-cli config set stop-writes-on-bgsave-error no
@@ -36,8 +36,29 @@ echo "cleaning up logs..."
 /opt/homebrew/bin/trash django.log celery.log
 
 echo "terminating processes..."
-kill $DJANGO_PID
-kill $CELERY_PID
-kill $TAIL_PID
+# kill $DJANGO_PID
+# kill $CELERY_PID
+# kill $TAIL_PID
+
+if [ -n "$DJANGO_PID" ] && ps -p $DJANGO_PID >/dev/null; then
+    echo "Terminating Django process with PID: $DJANGO_PID"
+    kill $DJANGO_PID
+else
+    echo "Django process with PID $DJANGO_PID is not running"
+fi
+
+if [ -n "$CELERY_PID" ] && ps -p $CELERY_PID >/dev/null; then
+    echo "Terminating Celery process with PID: $CELERY_PID"
+    kill $CELERY_PID
+else
+    echo "Celery process with PID $CELERY_PID is not running"
+fi
+
+if [ -n "$TAIL_PID" ] && ps -p $TAIL_PID >/dev/null; then
+    echo "Terminating tail process with PID: $TAIL_PID"
+    kill $TAIL_PID
+else
+    echo "Tail process with PID $TAIL_PID is not running"
+fi
 
 echo "done"
